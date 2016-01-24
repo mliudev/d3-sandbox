5fcfeb0c2961c24193b7291f360ded90e287977f
Mike Liu <mike.liu.dev@gmail.com>
2016-01-24 13:44:43 -0500

    Scales tutorial finished.

--- sandbox/js/graphs/demo.js
+++ sandbox/js/graphs/demo.js
@@ -1,18 +1,19 @@
 var DemoGraph = (function($) {
 
   var dataset = [];
-  var randMax = 500;
+  var randMax = 2000;
 
   var initData = function(numValues) {
     var xVals = [];
+    var random;
     for (var i = 0; i < numValues; i++) {
-      var random = Math.round(Math.random() * randMax);
+      random = Math.round(Math.random() * randMax);
       xVals.push(random);
     }
 
     var yVals = [];
-    for (var i = 0; i < numValues; i++) {
-      var random = Math.round(Math.random() * randMax);
+    for (var j = 0; j < numValues; j++) {
+      random = Math.round(Math.random() * randMax);
       yVals.push(random);
     }
 
@@ -24,25 +25,47 @@ var DemoGraph = (function($) {
   var init = function() {
     initData(100);
 
-    var svgWidth = 500;
-    var svgHeight = 500;
+    var svgWidth = 100;
+    var svgHeight = "500";
+    var yPadding = 20;
+    var xPadding = 1;
 
     var svg = d3.select("body")
                 .append("svg")
-                .attr("width", svgWidth)
+                .attr("width", svgWidth + "%")
                 .attr("height", svgHeight);
+
+    var xScale = d3.scale.linear()
+                    .domain([0, d3.max(dataset, function(d) {
+                        return d[0];
+                    })])
+                    .range([xPadding, svgWidth - xPadding * 7]);
+
+    var yScale = d3.scale.linear()
+                    .domain([0, d3.max(dataset, function(d) {
+                        return d[1];
+                    })])
+                    .range([svgHeight - yPadding, yPadding]);
+
+    var rScale = d3.scale.linear()
+                    .domain([0, d3.max(dataset, function(d) {
+                        return d[1];
+                    })])
+                    .range([2,5]);
 
     svg.selectAll("circle")
         .data(dataset)
         .enter()
         .append("circle")
         .attr("cx", function(d) {
-          return d[0];
+          return xScale(d[0]) +"%";
         })
         .attr("cy", function(d) {
-          return d[1];
+          return yScale(d[1]);
         })
-        .attr("r", 5);
+        .attr("r", function(d) {
+            return rScale(d[1]);
+        });
 
     svg.selectAll("text")
         .data(dataset)
@@ -52,10 +75,10 @@ var DemoGraph = (function($) {
           return d[0] + "," + d[1];
         })
         .attr("x", function(d) {
-          return d[0];
+          return xScale(d[0]) + "%";
         })
         .attr("y", function(d) {
-          return d[1];
+          return yScale(d[1]);
         });
   };
 
