53f1e29eade69df24b165551d89e4b255509a4d2
Mike Liu <mike.liu@nimbisservices.com>
2016-01-23 21:10:11 -0500

    Bar charts completed.

--- sandbox/js/graphs/demo.js
+++ sandbox/js/graphs/demo.js
@@ -1,46 +1,75 @@
 var DemoGraph = (function($) {
 
   var dataset = [];
-  var radiusMax = 20;
+  var randMax = 100;
 
-  var initData = function() {
-    for (var i = 0; i < 5; i++) {
-      var random = Math.round(Math.random() * radiusMax);
+  var initData = function(numValues) {
+    for (var i = 0; i < numValues; i++) {
+      var random = Math.round(Math.random() * randMax);
       dataset.push(random);
     }
   };
 
   var init = function() {
-    initData();
+    initData(100);
 
     var width = 500;
-    var height = 50;
+    var height = 500;
+
+    var barsInRow = 20;
+    var barWidth = (width - barsInRow) / barsInRow;
+    var maxBarHeight = randMax;
 
     var svg = d3.select("body")
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height);
 
-    circles = svg.selectAll("circle")
+    svg.selectAll("rect")
         .data(dataset)
         .enter()
-        .append("circle");
+        .append("rect")
+        .attr("x", function(d, i) {
+          xpos = i % barsInRow;
+          return xpos * (barWidth + 1);
+        })
+        .attr("y", function(d, i) {
+          ypos = i/barsInRow;
+          return (parseInt(ypos) + 1) * maxBarHeight - d;
+        })
+        .attr("width", barWidth)
+        .attr("height", function(d) {
+          return d;
+        })
+        .attr("fill", function(d) {
+          return "rgb(" + (d*3) + ",0,0)";
+        });
 
-    circles.attr("cx", function(d, i) {
-      return (i * 50) + 25;
-    })
-    .attr("cy", height/2)
-    .attr("r", function (d) {
-      return d;
-    })
-    .attr("fill", "green")
-    .attr("stroke", "red")
-    .attr("stroke-width", function(d) {
-      return d/2;
-    })
-    .attr("opacity", function(d) {
-      return d/radiusMax;
-    });
+    svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .text(function(d) {
+          return d;
+        })
+        .attr("x", function(d, i) {
+          xpos = i % barsInRow;
+          return xpos * (barWidth + 1) + barWidth/2;
+        })
+        .attr("y", function(d, i) {
+          ypos = i/barsInRow;
+          y = (parseInt(ypos) + 1) * maxBarHeight - d + 12;
+          if (d > 12) {
+            return y;
+          }
+          else {
+            return y - 13;
+          }
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white")
+        .attr("text-anchor", "middle");
   };
 
   return {
@@ -51,4 +80,4 @@ var DemoGraph = (function($) {
 
 $(function() {
   DemoGraph.init();
-})
+});
